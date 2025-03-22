3e35760ceda677af62841095660b765e8f14e003
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-03-22 18:08:46 -0400

    chore: add SE3 controller for CTBR mode

--- rotorpy/controllers/quadrotor_control.py
+++ rotorpy/controllers/quadrotor_control.py
@@ -3,7 +3,7 @@ from scipy.spatial.transform import Rotation
 
 class SE3Control(object):
     """
-    Quadrotor trajectory tracking controller based on https://ieeexplore.ieee.org/document/5717652 
+    Quadrotor trajectory tracking controller based on https://ieeexplore.ieee.org/document/5717652
 
     """
     def __init__(self, quad_params):
@@ -31,7 +31,7 @@ class SE3Control(object):
         self.rotor_pos       = quad_params['rotor_pos']
         self.rotor_dir       = quad_params['rotor_directions']
 
-        # Rotor parameters    
+        # Rotor parameters
         self.rotor_speed_min = quad_params['rotor_speed_min'] # rad/s
         self.rotor_speed_max = quad_params['rotor_speed_max'] # rad/s
 
@@ -50,7 +50,7 @@ class SE3Control(object):
                                  [self.Ixz, self.Iyz, self.Izz]]) # kg*m^2
         self.g = 9.81 # m/s^2
 
-        # Gains  
+        # Gains
         self.kp_pos = np.array([6.5,6.5,15])
         self.kd_pos = np.array([4.0, 4.0, 9])
         self.kp_att = 544
@@ -59,15 +59,15 @@ class SE3Control(object):
 
         # Linear map from individual rotor forces to scalar thrust and vector
         # moment applied to the vehicle.
-        k = self.k_m/self.k_eta  # Ratio of torque to thrust coefficient. 
+        k = self.k_m/self.k_eta  # Ratio of torque to thrust coefficient.
 
         # Below is an automated generation of the control allocator matrix. It assumes that all thrust vectors are aligned
         # with the z axis.
         self.f_to_TM = np.vstack((np.ones((1,self.num_rotors)),
-                                  np.hstack([np.cross(self.rotor_pos[key],np.array([0,0,1])).reshape(-1,1)[0:2] for key in self.rotor_pos]), 
+                                  np.hstack([np.cross(self.rotor_pos[key],np.array([0,0,1])).reshape(-1,1)[0:2] for key in self.rotor_pos]),
                                  (k * self.rotor_dir).reshape(1,-1)))
         self.TM_to_f = np.linalg.inv(self.f_to_TM)
-    
+
     def update(self, t, state, flat_output):
         """
         This function receives the current time, true state, and desired flat
@@ -93,14 +93,14 @@ class SE3Control(object):
             control_input, a dict describing the present computed control inputs with keys
                 cmd_motor_speeds, rad/s
                 cmd_motor_thrusts, N
-                cmd_thrust, N 
+                cmd_thrust, N
                 cmd_moment, N*m
                 cmd_q, quaternion [i,j,k,w]
                 cmd_w, angular rates in the body frame, rad/s
                 cmd_v, velocity in the world frame, m/s
                 cmd_acc, mass normalized thrust vector in the world frame, m/s/s.
 
-                Not all keys are used, it depends on the control_abstraction selected when initializing the Multirotor object. 
+                Not all keys are used, it depends on the control_abstraction selected when initializing the Multirotor object.
         """
         cmd_motor_speeds = np.zeros((4,))
         cmd_thrust = 0
@@ -147,7 +147,7 @@ class SE3Control(object):
         # Desired torque, in units N-m.
         u2 = self.inertia @ (-self.kp_att*att_err - self.kd_att*w_err) + np.cross(state['w'], self.inertia@state['w'])  # Includes compensation for wxJw component
 
-        # Compute command body rates by doing PD on the attitude error. 
+        # Compute command body rates by doing PD on the attitude error.
         cmd_w = -self.kp_att*att_err - self.kd_att*w_err
 
         # Compute motor speeds. Avoid taking square root of negative numbers.
@@ -171,5 +171,118 @@ class SE3Control(object):
                          'cmd_w':cmd_w,
                          'cmd_v':cmd_v,
                          'cmd_acc': cmd_acc}
-        
+
+        return control_input
+
+class SE3ControlCTBR(object):
+    """
+    Quadrotor trajectory tracking controller based on https://ieeexplore.ieee.org/document/5717652
+
+    """
+    def __init__(self, quad_params):
+        """
+        Parameters:
+            quad_params, dict with keys specified in rotorpy/vehicles
+        """
+
+        # Quadrotor physical parameters.
+        # Inertial parameters
+        self.mass = quad_params['mass'] # kg
+
+        self.g = 9.81 # m/s^2
+
+        # Gains
+        self.kp_pos = np.array([6.5,6.5,15])
+        self.kd_pos = np.array([4.0, 4.0, 9])
+        self.kp_att = 300.0
+        self.kd_att = 15.0
+
+    def update(self, t, state, flat_output):
+        """
+        This function receives the current time, true state, and desired flat
+        outputs. It returns the command inputs.
+
+        Inputs:
+            t, present time in seconds
+            state, a dict describing the present state with keys
+                x, position, m
+                v, linear velocity, m/s
+                q, quaternion [i,j,k,w]
+                w, angular velocity, rad/s
+            flat_output, a dict describing the present desired flat outputs with keys
+                x,        position, m
+                x_dot,    velocity, m/s
+                x_ddot,   acceleration, m/s**2
+                x_dddot,  jerk, m/s**3
+                x_ddddot, snap, m/s**4
+                yaw,      yaw angle, rad
+                yaw_dot,  yaw rate, rad/s
+
+        Outputs:
+            control_input, a dict describing the present computed control inputs with keys
+                cmd_motor_speeds, rad/s
+                cmd_motor_thrusts, N
+                cmd_thrust, N
+                cmd_moment, N*m
+                cmd_q, quaternion [i,j,k,w]
+                cmd_w, angular rates in the body frame, rad/s
+                cmd_v, velocity in the world frame, m/s
+                cmd_acc, mass normalized thrust vector in the world frame, m/s/s.
+
+                Not all keys are used, it depends on the control_abstraction selected when initializing the Multirotor object.
+        """
+        cmd_thrust = 0
+
+        def normalize(x):
+            """Return normalized vector."""
+            return x / np.linalg.norm(x)
+
+        def vee_map(S):
+            """Return vector corresponding to given skew symmetric matrix."""
+            return np.array([-S[1,2], S[0,2], -S[0,1]])
+
+        # Get the desired force vector.
+        pos_err  = state['x'] - flat_output['x']
+        dpos_err = state['v'] - flat_output['x_dot']
+        F_des = self.mass * (- self.kp_pos*pos_err
+                             - self.kd_pos*dpos_err
+                             + flat_output['x_ddot']
+                             + np.array([0, 0, self.g]))
+
+        # Desired thrust is force projects onto b3 axis.
+        R = Rotation.from_quat(state['q']).as_matrix()
+        b3 = R @ np.array([0, 0, 1])
+        u1 = np.dot(F_des, b3)
+
+        # Desired orientation to obtain force vector.
+        b3_des = normalize(F_des)
+        yaw_des = flat_output['yaw']
+        c1_des = np.array([np.cos(yaw_des), np.sin(yaw_des), 0])
+        b2_des = normalize(np.cross(b3_des, c1_des))
+        b1_des = np.cross(b2_des, b3_des)
+        R_des = np.stack([b1_des, b2_des, b3_des]).T
+
+        # Orientation error.
+        S_err = 0.5 * (R_des.T @ R - R.T @ R_des)
+        att_err = vee_map(S_err)
+
+        # Angular velocity error (this is oversimplified).
+        w_des = np.array([0, 0, flat_output['yaw_dot']])
+        w_err = state['w'] - w_des
+
+        # Compute command body rates by doing PD on the attitude error.
+        cmd_w = -self.kp_att*att_err - self.kd_att*w_err
+
+        # Assign controller commands.
+        cmd_thrust = u1                                             # Commanded thrust, in units N.
+
+        control_input = {'cmd_motor_speeds':np.zeros((4,)),
+                         'cmd_motor_thrusts':np.zeros((4,)),
+                         'cmd_thrust':cmd_thrust,
+                         'cmd_moment':np.zeros((3,)),
+                         'cmd_q':np.array([0,0,0,1]),
+                         'cmd_w':cmd_w,
+                         'cmd_v':np.zeros((3,)),
+                         'cmd_acc': np.zeros((3,))}
+
         return control_input
